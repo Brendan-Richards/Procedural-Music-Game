d60d08eda16db6063c11a7c793d70b05dc0ccb7b
Brendan <brendanerichards@comcast.net>
2020-10-20 17:01:36 -0600

    added a few more trees and finished tree/shrub placement

--- src/Foliage.ts
+++ src/Foliage.ts
@@ -11,33 +11,48 @@ const flatFoliage = (scene: MountainScene, x1: number, x2: number, y: number) =>
     console.log('numTiles:', numTiles, 'numTrees:', numTrees);
 
     let treeCoordinates: [number[]] = [];
+    let currentTreeTypes: string[] = [];
 
     for(let i=0; i < numTrees; i++){
+
+        if(currentTreeTypes.length===11){
+            break;
+        }
+
         let location = 0;
         let validLocation = false;
+        const buffer = 30;
         while(!validLocation){
-            location = Math.floor(Math.random() * (x2-x1)) + x1;
+            location = Math.floor(Math.random() * (x2-x1-buffer)) + x1;
+            if(location + 120 > x2){// location is too close to right edge of flat section
+                continue;
+            }
             validLocation = true;
-            for(let j=0; j<treeCoordinates.length; j++){
+            for(let j=0; j<treeCoordinates.length; j++){ 
                 if(location < treeCoordinates[j][1] && location > treeCoordinates[j][0] ||
-                   location + 25 < treeCoordinates[j][1] && location + 25 > treeCoordinates[j][0]){
+                   location + buffer < treeCoordinates[j][1] && location + buffer > treeCoordinates[j][0]){
                     validLocation = false;
                     break;
                 }
             }
         }
-        treeCoordinates.push([location-2, location+23]);
-        createTree(scene, location, y - scene.maxGameHeight, 1, 1);
+        treeCoordinates.push([location-2, location+buffer]);
+        createTree(scene, location, y - scene.maxGameHeight, 1, 1, currentTreeTypes);
         console.log('putting tree at x:', location, 'y:', y);
-    }
+    }   
 
+    placeBushes(scene, x1, x2, y);
+}
+
+
+const placeBushes = (scene: MountainScene, x1: number, x2: number, y: number) => {
     //number of shrubs to place here
-    const maxShrubs = 3;
+    const maxShrubs = 5;
     const numShrubs = Math.floor(Math.random() * maxShrubs);
-    console.log('placing', numShrubs, 'shrubs');
+    //console.log('placing', numShrubs, 'shrubs');
     const flatWidth = (x2-x1);
-    console.log('flatWidth:', flatWidth);
-    
+    //console.log('flatWidth:', flatWidth);
+
     //minimum width to place shrubs is 190 because shrub0.png has width of ~190
     if(flatWidth >= 190){
         for(let i=0; i<numShrubs; i++){
@@ -49,126 +64,42 @@ const flatFoliage = (scene: MountainScene, x1: number, x2: number, y: number) =>
                 shrubWidth = scene.cache.json.get('environmentAtlas').frames[shrubName].spriteSourceSize.w;
             }
 
-            console.log('shrub name:', shrubName);
-            console.log(shrubWidth);
+            //console.log('shrub name:', shrubName);
+            //console.log(shrubWidth);
             const locationX = Math.floor(Math.random() * (x2-shrubWidth-x1)) + x1;
             const sh = scene.add.image(locationX, y, 'environmentAtlas', shrubName).setOrigin(0,1);
-            console.log('placed shrub at:', locationX, y);
+            //console.log('placed shrub at:', locationX, y);
 
         }
     }
-
-
-
-        
-        //scene.add.image(x1, y, 'environmentAtlas', shrubName).setOrigin(0,1);
-
-    }
 }
 
-const createTree = (scene: MountainScene, x: number, y: number, scrollFactorX: number, scrollFactorY: number): void => {
+const createTree = (scene: MountainScene, x: number, y: number, scrollFactorX: number, scrollFactorY: number, otherTrees: string[]): void => {
         const scrollAmount = scene.maxGameHeight - scene.cameras.main.height;
         const offsetY = scrollFactorY * scrollAmount + scene.cameras.main.height;
 
-        const randomOffsetY = Math.floor(Math.random() * 20);
+        const randomOffsetY = Math.floor(Math.random() * 20) + 5;
         let treeType = 'tree_premade';
-        let maxTreeNum = 8;
+        let maxTreeNum = 11;
 
         if(Math.random() < 0.15){
             treeType = 'tree_micro';
             maxTreeNum = 4;
         }
 
-        const treeNum = Math.floor(Math.random() * maxTreeNum).toString();
-        const stump = scene.add.image(x, offsetY + y + randomOffsetY, 'environmentAtlas', treeType + treeNum)
+        let treeNum = Math.floor(Math.random() * maxTreeNum).toString();
+
+        while(treeType==='tree_premade' && otherTrees.includes(treeType + treeNum)){
+            treeNum = Math.floor(Math.random() * maxTreeNum).toString();
+        }
+
+        scene.add.image(x, offsetY + y + randomOffsetY, 'environmentAtlas', treeType + treeNum)
             .setScrollFactor(scrollFactorX, scrollFactorY)
             .setOrigin(0,1);
+
+        otherTrees.push(treeType + treeNum);
 }
 
-// const createTree = (scene: MountainScene, x: number, y: number, scrollFactorX: number, scrollFactorY: number): void => {
-//     const scrollAmount = scene.maxGameHeight - scene.cameras.main.height;
-//     const offsetY = scrollFactorY * scrollAmount + scene.cameras.main.height;
-
-//     // y += scrollFactorY * this.scene.cameras.main.height;
-
-//     const stump = scene.add.sprite(x, offsetY + y , 'environmentAtlas', 'tree_skinny_stump1')
-//         .setScrollFactor(scrollFactorX, scrollFactorY)
-//         .setOrigin(0,1);
-//     y -= stump.height;
-
-//     console.log('setting bottom left coordinate of stump:', x, offsetY + y)
-
-//     const stumpAngle = 5;
-//     let stumpOffsetX = Math.ceil(Math.sin(stumpAngle/180 * Math.PI) * stump.height); 
-//     let stumpOffsetY = Math.ceil(stump.height * (1 - Math.cos(stumpAngle/180 * Math.PI)));
-
-//     console.log('stumpOffsetY is:', stumpOffsetY);
-//     console.log('bottom left coordinate of stump:', stump.getBottomLeft().x, stump.getBottomLeft().y)
-
-//     //stump;
-
-//     if(stumpAngle < 0){
-//         stump.setY(stump.getBottomLeft().y + stumpOffsetY);
-//         //stumpOffsetY *= 2;
-//     }
-//     else if(stumpAngle > 0){
-//     //    stump.setOrigin(1,1).setX(stump.getBottomRight().x);
-//         //stump.setOrigin(1,1).setY(stump.getBottomRight().y + stumpOffsetY);
-//        // stumpOffsetY += stumpOffsetY;
-//     }
-//     stumpOffsetY *= 2;
-//     stump.angle = stumpAngle;
-    
-
-//     let bodyOffsetX = 0;
-//     let bodyOffsetY = 0;
-//     let bodyAngle = stumpAngle;
-//     const maxBodyPieces = 4;
-//     //const numBodyPieces = Math.floor(Math.random() * maxBodyPieces) + 1;
-//     const numBodyPieces = 1;
-//     for(let i = 0; i < numBodyPieces; i++){
-//         // const body = scene.add.image(x + stumpOffsetX + bodyOffsetX, offsetY + y + 2 * stumpOffsetY - bodyOffsetY, 'environmentAtlas', 'tree_skinny_body')
-//         //     .setScrollFactor(scrollFactorX, scrollFactorY)
-//         //     .setOrigin(0,1); 
-//         let body;
-//         if(bodyAngle > 0){
-//             body = scene.add.image(x + stumpOffsetX, offsetY + y + stumpOffsetY, 'environmentAtlas', 'tree_skinny_body')
-//                 .setScrollFactor(scrollFactorX, scrollFactorY)
-//                 .setOrigin(0,1); 
-//             body.angle = bodyAngle;
-//         }
-//         else{
-//             body = scene.add.image(x + stumpOffsetX, offsetY + y + stumpOffsetY, 'environmentAtlas', 'tree_skinny_body')
-//                 .setScrollFactor(scrollFactorX, scrollFactorY)
-//                 .setOrigin(0,1); 
-//         }
-
-
-//         //body.angle = stumpAngle;
-
-//         bodyOffsetX += Math.ceil(Math.sin(bodyAngle/180 * Math.PI) * body.height); 
-//         //bodyOffsetY += Math.ceil(body.width * Math.sin(bodyAngle/180 * Math.PI));
-//         bodyOffsetY += Math.ceil(body.height * (1 - Math.cos(bodyAngle/180 * Math.PI)));
-
-//         const newBodyAngle = 5;
-
-//         // if(newBodyAngle > 0 && bodyAngle < 0){
-//         //     stump.setY(stump.getBottomLeft().y - stumpOffsetY);
-//         //     stumpOffsetY *= 2;
-//         // }
-    
-//         // stump.angle = stumpAngle;
-        
-//         y -= body.height; 
-//     }
-
-//     //console.log('putting tree top at:', x, y);
-//     const bodyTopOffset = -7;
-//     scene.add.image(x + bodyTopOffset + stumpOffsetX + bodyOffsetX, offsetY + y + stumpOffsetY + bodyOffsetY, 'environmentAtlas', 'tree_skinny_top1')
-//         .setScrollFactor(scrollFactorX, scrollFactorY)
-//         .setOrigin(0,1);        
-     
-// }
 
 const verticalFoliage = () => {
 
