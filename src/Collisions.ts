cb1f733491be2878d90f7fa7c57dec8e5336f4f7
Brendan Richards <brendanerichards@comcast.net>
2020-11-05 11:14:36 -0700

    dont allow any top wall logic if player is attacking

--- src/Collisions.ts
+++ src/Collisions.ts
@@ -150,105 +150,113 @@ export default (scene: MountainScene): void => {
                         //console.log(collisionNormal);
                         
                         if(scene.currentPlayerAnimation==='attack1' || scene.currentPlayerAnimation==='attack2' || scene.currentPlayerAnimation==='attack3' ||
-                        scene.currentPlayerAnimation==='airAttack1' || scene.currentPlayerAnimation==='airAttack2'){
-                            scene.audio.attackSound.stop();
-                            scene.audio.swordRockImpact.play(scene.audio.swordRockImpactConfig);
-                        }
-
-                        console.log(collisionNormal);
-                        if(Math.abs(Math.round(collisionNormal.x))===0 && Math.abs(Math.round(collisionNormal.y))===1){
-                
-                            console.log('collided with top of topWall block');
-
-                            const currentTime = scene.time.now;
-                            if(!scene.playerCanJump && currentTime - scene.lastLandingTime > 100){
-                                scene.audio.hardLanding.play(scene.audio.hardLandingConfig);
+                            scene.currentPlayerAnimation==='airAttack1' || scene.currentPlayerAnimation==='airAttack2'){
+                            if(!scene.swordCollided){
+                                scene.audio.attackSound.stop();
+                                scene.audio.swordRockImpact.play(scene.audio.swordRockImpactConfig);
+                                const factor = scene.currentPlayerDirection==='left' ? 1 : -1;
+                                scene.player.setVelocityX(factor * scene.playerSpeed);
+                                scene.swordCollided = true;
                             }
-                    
-                           if(scene.losingStamina){
-                                scene.losingStamina = false;
-                                scene.gainingStamina = true;
-                            }
-
-                            scene.playerLastOnGroundTime = scene.time.now;
-                            scene.playerCanJump = true;
-                            scene.playerWallSliding = false;
-                            if(scene.playerRampSliding){
-                                scene.playerFlatSliding = true;
-                                scene.playerRampSliding = false;
-                                scene.setFlatSlide = false;
-                            } 
                         }
                         else{
-                            console.log('collided with side of topWall block');
-
-                            //scene.add.circle(collisionPoint.x, collisionPoint.y, 2, 0xff0000).setDepth(100);
-                            
-                            //scene.playerFriction = 0.01;
-                            //console.log(event, body1, body2);
-
-                            if(!scene.playerLedgeClimb){
-                                scene.ledgePosition = other.body.position;
-                                //scene.add.circle(scene.ledgePosition.x, scene.ledgePosition.y, 2, 0xffff00).setDepth(100);
-                                //console.log('other.tile:', other.tile);
-                                let grabPositionX;
-                                if(collisionPoint.x > scene.ledgePosition.x){
-                                    //the 4 convex top walls
-                                    if([10, 12, 100, 102].includes(other.tile.index)){
-                                        grabPositionX = other.tile.pixelX + 64 + 3;
-                                    }
-                                    else{// the 4 concave top walls
-                                        grabPositionX = other.tile.pixelX + - 13 + 64;
-                                    }
-                                    scene.currentPlayerDirection = 'left';
+                            console.log(collisionNormal);
+                            if(Math.abs(Math.round(collisionNormal.x))===0 && Math.abs(Math.round(collisionNormal.y))===1){
+                    
+                                console.log('collided with top of topWall block');
+    
+                                const currentTime = scene.time.now;
+                                if(!scene.playerCanJump && currentTime - scene.lastLandingTime > 100){
+                                    scene.audio.hardLanding.play(scene.audio.hardLandingConfig);
                                 }
-                                else{
-                                    if([10, 12, 100, 102].includes(other.tile.index)){
-                                        grabPositionX = other.tile.pixelX;
-                                    }
-                                    else{// the 4 concave top walls
-                                        grabPositionX = other.tile.pixelX + 15;
-                                    }
-                                    scene.currentPlayerDirection = 'right';
-                                };
-
-                                const grabPositionY = other.tile.pixelY + 16;
-                                const buffer = 20;
-
-                                //if we collided with the outward facing side of the tile block
-                                if(collisionPoint.x > scene.ledgePosition.x && [125, 102, 35, 12].includes(other.tile.index) ||
-                                   collisionPoint.x < scene.ledgePosition.x && [10, 33, 100, 123].includes(other.tile.index)){
-                                    if(grabPositionY - 2 * buffer < scene.player.body.position.y && scene.player.body.position.y < grabPositionY + buffer && scene.stamina > 0){
-                                        scene.playerLedgeGrab = true;
-                                        if(!scene.staminaActive){
-                                            scene.drawStamina();
-                                            scene.losingStamina = true; 
+                        
+                               if(scene.losingStamina){
+                                    scene.losingStamina = false;
+                                    scene.gainingStamina = true;
+                                }
+    
+                                scene.playerLastOnGroundTime = scene.time.now;
+                                scene.playerCanJump = true;
+                                scene.playerWallSliding = false;
+                                if(scene.playerRampSliding){
+                                    scene.playerFlatSliding = true;
+                                    scene.playerRampSliding = false;
+                                    scene.setFlatSlide = false;
+                                } 
+                            }
+                            else{
+                                console.log('collided with side of topWall block');
+    
+                                //scene.add.circle(collisionPoint.x, collisionPoint.y, 2, 0xff0000).setDepth(100);
+                                
+                                //scene.playerFriction = 0.01;
+                                //console.log(event, body1, body2);
+    
+                                if(!scene.playerLedgeClimb){
+                                    scene.ledgePosition = other.body.position;
+                                    //scene.add.circle(scene.ledgePosition.x, scene.ledgePosition.y, 2, 0xffff00).setDepth(100);
+                                    //console.log('other.tile:', other.tile);
+                                    let grabPositionX;
+                                    if(collisionPoint.x > scene.ledgePosition.x){
+                                        //the 4 convex top walls
+                                        if([10, 12, 100, 102].includes(other.tile.index)){
+                                            grabPositionX = other.tile.pixelX + 64 + 3;
                                         }
-                                        scene.gainingStamina = false;
-                                        scene.player.setPosition(grabPositionX, grabPositionY);
-                                        scene.matter.setVelocity(scene.player.body as Phaser.Types.Physics.Matter.MatterBody, 0, 0);
-                                        scene.playerWallSliding = false;
+                                        else{// the 4 concave top walls
+                                            grabPositionX = other.tile.pixelX + - 13 + 64;
+                                        }
+                                        scene.currentPlayerDirection = 'left';
                                     }
                                     else{
-                                        scene.playerFriction = scene.stamina > 0 ? 0.3 : 0;
-                                        scene.playerWallSliding = true;
+                                        if([10, 12, 100, 102].includes(other.tile.index)){
+                                            grabPositionX = other.tile.pixelX;
+                                        }
+                                        else{// the 4 concave top walls
+                                            grabPositionX = other.tile.pixelX + 15;
+                                        }
+                                        scene.currentPlayerDirection = 'right';
+                                    };
+    
+                                    const grabPositionY = other.tile.pixelY + 16;
+                                    const buffer = 20;
+    
+                                    //if we collided with the outward facing side of the tile block
+                                    if(collisionPoint.x > scene.ledgePosition.x && [125, 102, 35, 12].includes(other.tile.index) ||
+                                       collisionPoint.x < scene.ledgePosition.x && [10, 33, 100, 123].includes(other.tile.index)){
+                                        if(grabPositionY - 2 * buffer < scene.player.body.position.y && scene.player.body.position.y < grabPositionY + buffer && scene.stamina > 0){
+                                            scene.playerLedgeGrab = true;
+                                            if(!scene.staminaActive){
+                                                scene.drawStamina();
+                                                scene.losingStamina = true; 
+                                            }
+                                            scene.gainingStamina = false;
+                                            scene.player.setPosition(grabPositionX, grabPositionY);
+                                            scene.matter.setVelocity(scene.player.body as Phaser.Types.Physics.Matter.MatterBody, 0, 0);
+                                            scene.playerWallSliding = false;
+                                        }
+                                        else{
+                                            scene.playerFriction = scene.stamina > 0 ? 0.3 : 0;
+                                            scene.playerWallSliding = true;
+                                        }
                                     }
+                                    else{
+                                        scene.playerCanJump = true;
+                                    }
+    
+                                    // if(scene.playerLedgeGrab){
+                                    //     scene.losingStamina = true;
+                                    // }
+    
+                                    scene.playerRampSliding = false;
+                                    scene.playerFlatSliding = false;
+                                    //scene.playerCanJump = false;
+                                    scene.playerWallJumping = false;
                                 }
-                                else{
-                                    scene.playerCanJump = true;
-                                }
-
-                                // if(scene.playerLedgeGrab){
-                                //     scene.losingStamina = true;
-                                // }
-
-                                scene.playerRampSliding = false;
-                                scene.playerFlatSliding = false;
-                                //scene.playerCanJump = false;
-                                scene.playerWallJumping = false;
+    
                             }
-
                         }
+
+
                     }
                 }
             }
